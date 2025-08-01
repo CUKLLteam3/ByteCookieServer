1738e82ec445317c7e5606bebb3fcdaa3fe61f95
KyungHoon03 <tw21lkh@naver.com>
2025-08-01 15:39:04 +0900

    first project setting

---
+++ src/main/java/com/example/bytecookie/ByteCookieApplication.java
@@ -0,0 +1,13 @@
+package com.example.bytecookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteCookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteCookieApplication.class, args);
+    }
+
+}
