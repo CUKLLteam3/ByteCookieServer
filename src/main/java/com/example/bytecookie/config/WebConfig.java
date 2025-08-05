278eb294b1760011ff1c4b65b5a5412c9d5ddb3a
KyungHoon03 <tw21lkh@naver.com>
2025-08-05 21:50:36 +0900

    feat/AI all make

--- src/main/java/com/example/bytecookie/config/WebConfig.java
+++ src/main/java/com/example/bytecookie/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://api-bytecookie.click")
+                .allowedOrigins("http://localhost:8080","https://api-bytecookie.click")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
