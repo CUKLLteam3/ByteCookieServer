56b1e2c796ffe8903653499e780955a19e62d531
KyungHoon03 <tw21lkh@naver.com>
2025-08-18 12:38:07 +0900

    plus config front local

--- src/main/java/com/example/bytecookie/config/WebConfig.java
+++ src/main/java/com/example/bytecookie/config/WebConfig.java
@@ -9,8 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080","https://api-bytecookie.click")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "http://localhost:8080",
+                        "https://api-bytecookie.click")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"
+                )
                 .allowedHeaders("*")
                 .allowCredentials(false);
     }
