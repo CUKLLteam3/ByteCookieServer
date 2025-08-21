7fc7f0e16ddf0df1cac7d7442250b92ae627de18
Baekhojin <baeghojin4@gmail.com>
2025-08-22 01:33:23 +0900

    DB 통합

--- src/main/java/com/example/bytecookie/domain/user/repository/UserInfoRepository.java
+++ src/main/java/com/example/bytecookie/domain/user/repository/UserInfoRepository.java
@@ -1,9 +1,10 @@
 package com.example.bytecookie.domain.user.repository;
 
-
+/*
 import com.example.bytecookie.domain.user.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     // 예: Optional<UserInfo> findByEmail(String email); 등 추가 가능
 }
+*/
